2f092af7623d3320d56a0f854a7d7f0d131df5a8
rasyid annas <rasshit.dsgn@gmail.com>
2024-03-28 07:14:36 +0700

    3/1. Reactivity.mp4

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -7,13 +7,16 @@
    export let text = 'Button'
    export let title
 
+   // reactiviy has reactive value / when value change it will re-render
+   $: extendText = text + '!!!'
+
    const changeText= () => {
       text = "Button Clicked"
       dispatch('addMsg', text)
    }
 </script>
 
-<button {title} on:click={changeText}>{text}</button>
+<button {title} on:click={changeText}>{extendText}</button>
 
 <style>
    button{
--- src/components/Messages.svelte
+++ src/components/Messages.svelte
@@ -1,20 +1,22 @@
 <script>
    export let newMessage = "No click yet";
-   
+
    let messages = ['msg1', 'msg2', 'msg3', 'msg4', 'msg5', 'msg6']
-   const msgPromise = new Promise((resolve, reject) => {
+   $: msgPromise = new Promise((resolve, reject) => {
       setTimeout(() => {
          resolve(messages)
          reject('Could not load the messages')
       }, 2000)
    })
+
+   $: messages = [...messages, newMessage]
 </script>
 
 <!--Async Block-->
 {#await msgPromise}
    <p>Loading...</p>
 {:then messages}
-   <p>{messages} {newMessage}</p>
+   <p>{messages}</p>
 {:catch error}
    <p>{error}</p>
 {/await}
