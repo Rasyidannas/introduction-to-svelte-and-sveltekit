73ffccf115da895e654e7eb32fab3dc6c28a7914
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-02 10:17:23 +0700

    4/7. Some Styling...mp4

--- catfacts/src/App.svelte
+++ catfacts/src/App.svelte
@@ -4,6 +4,39 @@
 </script>
 
 <main>
-	<Settings />
-	<Feed />
+	<div id="container">
+		<div id="settings">
+			<Settings />
+		</div>
+
+		<div id="feed">
+			<Feed />
+		</div>
+	</div>
 </main>
+
+<style>
+	main {
+		text-align: center;
+		margin: 0 auto;
+		padding: 1em;
+	}
+
+	#container {
+		border: 1px solid black;
+		padding: 10px;
+		min-height: 500px;
+		display: flex;
+	}
+
+	#settings {
+		float: center;
+		border: 1px solid grey;
+		border-radius: 10px;
+		padding: 5px;
+	}
+
+	#feed {
+		margin: 0 auto;
+	}
+</style>
