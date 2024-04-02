3ddb7a82bc3b93bdb78f16fb35155db50d6eeac2
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-02 09:53:05 +0700

    4/6. The Fact COmponent.mp4

--- catfacts/src/Feed.svelte
+++ catfacts/src/Feed.svelte
@@ -1,5 +1,26 @@
 <script>
     import feedStore from "./feedStore.js";
+    import Fact from "./Fact.svelte";
+
+    let factArray = [];
+
+    $: {
+        factArray.pop()
+
+        //this will added the amount of facts to the array when range input is increased
+        while($feedStore.amount > factArray.length) {
+            factArray.unshift(`That is fact!`);
+        }
+
+        factArray = [...factArray]
+    }
 </script>
 
 <h2>Recent {$feedStore.amount} Facts</h2>
+<ol>
+    {#each factArray as fact }
+        <li>
+            <Fact fact={fact}/>
+        </li>
+    {/each}
+</ol>
