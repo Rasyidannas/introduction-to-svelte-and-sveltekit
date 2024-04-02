4a67e0ff3a2e74de84be0ff7a71298617d581a4a
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-03 06:29:22 +0700

    4/8. Fetching with API Calls.mp4

--- catfacts/src/Feed.svelte
+++ catfacts/src/Feed.svelte
@@ -5,11 +5,23 @@
     let factArray = [];
 
     $: {
-        factArray.pop()
+        updateFacts($feedStore.amount);
+    }
+
+    const getRandomFact = async () => {
+        const response = await fetch("https://catfact.ninja/fact");
+        const data = await response.json();
+        return data.fact;
+    }
+
+    const updateFacts = async (amount) => {
+        if(amount < factArray.length) {
+            factArray.pop()
+        }
 
         //this will added the amount of facts to the array when range input is increased
-        while($feedStore.amount > factArray.length) {
-            factArray.unshift(`That is fact!`);
+        while(amount > factArray.length) {
+            factArray.unshift(await getRandomFact());
         }
 
         factArray = [...factArray]
