fab2200d8758acd2b367156e843c9bd8d5ae4ba7
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-03 07:33:22 +0700

    5/2. Tick.mp4

--- catfacts/src/Child.svelte
+++ catfacts/src/Child.svelte
@@ -1,6 +1,16 @@
 <script>
-    import {onMount, onDestroy, beforeUpdate, afterUpdate} from 'svelte'
+    import {onMount, onDestroy, beforeUpdate, afterUpdate, tick} from 'svelte'
     let text = 'Child Showing'
+    let x = 0
+
+    $: y = x + 1
+
+    const xy = async () => {
+        x++;
+        await tick() //this will wait component data updates until they are rendered
+        console.log('x is ', x);
+        console.log('y is ', y);
+    }
 
     onMount(() => console.log('mounted')) //this running after component rendered in the DOM, should be used for async methods
     onDestroy(() => console.log('destroyed')) //this running after DOM element is destroyed
@@ -11,3 +21,4 @@
 
 <p>{text}</p>
 <input bind:value={text}>
+<button on:click={xy}>tick</button>
