ff3bca42eb7c413c27146d7585067c6bd6fbc929
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-03 21:01:12 +0700

    6/2. SvelteBody.mp4

--- catfacts/src/App.svelte
+++ catfacts/src/App.svelte
@@ -1,8 +1,6 @@
 <script>
 	let name = 'world';
-	let innerWidth
 </script>
 
 <h1>Hello {name}!</h1>
-<p>{innerWidth}</p>
-<svelte:window on:click={() => console.log('window clicked')} bind:innerWidth />
+<svelte:body on:mousemove={(e) => console.log("mouse is at", e.clientX, e.clientY)} />
