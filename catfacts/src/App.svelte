5a02f479fa84c91b1ba72d7cc3823e2ce3e193de
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-03 21:05:27 +0700

    6/3. SvelteHead.mp4

--- catfacts/src/App.svelte
+++ catfacts/src/App.svelte
@@ -2,5 +2,8 @@
 	let name = 'world';
 </script>
 
-<h1>Hello {name}!</h1>
-<svelte:body on:mousemove={(e) => console.log("mouse is at", e.clientX, e.clientY)} />
+<h1 class="text-light bg-dark">Hello {name}!</h1>
+
+<svelte:head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</svelte:head>
