81df875a7c2ca54393b6f74f44203a1fd6af5fbf
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-04 10:07:55 +0700

    7/2. Routing.mp4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <script>
-   import { setContext } from "svelte";
-   import Button from "../components/Button.svelte";
-   import Messages from "../components/Messages.svelte";
-
-   //this for set Context API
-   setContext('exclamation', '!');
+   
 </script>
 
 <h1>This is homepage</h1>
