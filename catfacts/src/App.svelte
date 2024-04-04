e3f2a3782cd08a6f5349845d4a6931ee3fa75bed
Rasyidannas <rasshit.dsgn@gmail.com>
2024-04-04 08:47:17 +0700

    6/7. Slot.mp4

--- catfacts/src/App.svelte
+++ catfacts/src/App.svelte
@@ -1,7 +1,11 @@
 <script>
-	import Count from "./Count.svelte";
+	import Child from "./Child.svelte";
 	let name = 'world';
 </script>
 
 <h1 class="text-light bg-dark">Hello {name}!</h1>
-<Count />
+<Child>
+	<p>Example 1</p>
+	<p slot="ex2">Example 2</p>
+	<p>Example 3</p>
+</Child>
---
+++ catfacts/src/Child.svelte
@@ -0,0 +1,12 @@
+<div>
+    <h2>Div for slots</h2>
+    <slot/>
+    <slot name="ex2"/>
+</div>
+
+<style>
+    div {
+        border: 1px solid black;
+        padding: 10px;
+    }
+</style>
